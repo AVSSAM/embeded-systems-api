203ea63352fbf69fc4917cbd4495aac616cb4aef
AVSSAM <sm201211d@gmail.com>
2021-02-04 15:13:33 +0530

    test commit

--- src/main/java/Group10/example/API/Repository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package Group10.example.API.Repository;
-
-import Group10.example.API.Model.Admin;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface AdminRepository extends MongoRepository<Admin,String> {
-
-    public Admin findByuserName(String userName);
-}
