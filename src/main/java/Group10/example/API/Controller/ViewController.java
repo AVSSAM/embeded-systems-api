50041d77242de086765ed7dbb9f1508fee7bf9ca
AVSSAM <sm201211d@gmail.com>
2021-02-04 10:24:14 +0530

    initail commit

---
+++ src/main/java/Group10/example/API/Controller/ViewController.java
@@ -0,0 +1,17 @@
+package Group10.example.API.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+
+@RestController
+public class ViewController {
+
+    @GetMapping("/")
+   public String home() {
+       return "hi";
+    }
+
+}
